8dadb86a429a8a025a05ed970c4c6ac7f33cfbcc
Mayank Wadhwa <mayankwadhwa@outlook.in>
2019-05-13 12:38:51 +0530

    email element added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,25 +3,28 @@ import org.w3c.dom.HTMLDivElement
 import org.w3c.dom.HTMLHeadingElement
 import org.w3c.dom.HTMLImageElement
 import kotlin.browser.document
-import kotlin.dom.addClass
 
 private val content = document.getElementById("content") as HTMLDivElement
 
 fun main() {
 
+    val topContainerElement = document.createElement("div") as HTMLDivElement
+    val textContainerElement = document.createElement("div") as HTMLDivElement
     val imageElement = document.createElement("img") as  HTMLImageElement
     val titleElement = document.createElement("h1") as HTMLHeadingElement
     val designationElement = document.createElement("h3") as HTMLHeadingElement
+    val emailElement = document.createElement("h3") as HTMLHeadingElement
 
 
     imageElement.src = "https://media.licdn.com/dms/image/C5603AQGWF8Lu85Cmaw/profile-displayphoto-shrink_200_200/0?e=1560988800&v=beta&t=dF6DPHFx31ndEOYQ3_X211di58ZPrw1ix2Ve_XNaPAw"
     titleElement.innerHTML = "Mayank Wadhwa"
     designationElement.innerHTML = "Mobile and Frontend Engineer"
+    emailElement.innerHTML = "mayank.wadhwa@outlook.in"
 
+    textContainerElement.appendChild(imageElement, titleElement, designationElement, emailElement)
 
+    content.appendChild(textContainerElement)
 
-    content.appendChild(imageElement, titleElement, designationElement)
-    content.addClass("root")
 
 }
 
--- web/MyGithubProfile.js
+++ web/MyGithubProfile.js
@@ -4,18 +4,21 @@ if (typeof kotlin === 'undefined') {
 var MyGithubProfile = function (_, Kotlin) {
   'use strict';
   var throwCCE = Kotlin.throwCCE;
-  var addClass = Kotlin.kotlin.dom.addClass_hhb33f$;
   var content;
   function main() {
-    var tmp$, tmp$_0, tmp$_1;
-    var imageElement = Kotlin.isType(tmp$ = document.createElement('img'), HTMLImageElement) ? tmp$ : throwCCE();
-    var titleElement = Kotlin.isType(tmp$_0 = document.createElement('h1'), HTMLHeadingElement) ? tmp$_0 : throwCCE();
-    var designationElement = Kotlin.isType(tmp$_1 = document.createElement('h3'), HTMLHeadingElement) ? tmp$_1 : throwCCE();
+    var tmp$, tmp$_0, tmp$_1, tmp$_2, tmp$_3, tmp$_4;
+    var topContainerElement = Kotlin.isType(tmp$ = document.createElement('div'), HTMLDivElement) ? tmp$ : throwCCE();
+    var textContainerElement = Kotlin.isType(tmp$_0 = document.createElement('div'), HTMLDivElement) ? tmp$_0 : throwCCE();
+    var imageElement = Kotlin.isType(tmp$_1 = document.createElement('img'), HTMLImageElement) ? tmp$_1 : throwCCE();
+    var titleElement = Kotlin.isType(tmp$_2 = document.createElement('h1'), HTMLHeadingElement) ? tmp$_2 : throwCCE();
+    var designationElement = Kotlin.isType(tmp$_3 = document.createElement('h3'), HTMLHeadingElement) ? tmp$_3 : throwCCE();
+    var emailElement = Kotlin.isType(tmp$_4 = document.createElement('h3'), HTMLHeadingElement) ? tmp$_4 : throwCCE();
     imageElement.src = 'https://media.licdn.com/dms/image/C5603AQGWF8Lu85Cmaw/profile-displayphoto-shrink_200_200/0?e=1560988800&v=beta&t=dF6DPHFx31ndEOYQ3_X211di58ZPrw1ix2Ve_XNaPAw';
     titleElement.innerHTML = 'Mayank Wadhwa';
     designationElement.innerHTML = 'Mobile and Frontend Engineer';
-    appendChild(content, [imageElement, titleElement, designationElement]);
-    addClass(content, ['root']);
+    emailElement.innerHTML = 'mayank.wadhwa@outlook.in';
+    appendChild(textContainerElement, [imageElement, titleElement, designationElement, emailElement]);
+    content.appendChild(textContainerElement);
   }
   function appendChild($receiver, elements) {
     var tmp$;
